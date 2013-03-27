574bc696f256ec9e576e463c9f4bb5cf8fa38f55
drebelox <daniel@bodhi5.com>
2013-03-26 23:49:08 -0700

    gallery update

--- js/custom.js
+++ js/custom.js
@@ -1,37 +1,37 @@
 /* Author: Umair Khan @ Pixel Art Inc */
 
 jQuery(document).ready(function() {
-		
-		
-		
+
+
+
 		// Slider
-		$('.slider .slides').cycle({		
-			fx: 		 'fade',		
+		$('.slider .slides').cycle({
+			fx: 		 'fade',
 			next:		 '.next',
 			prev: 		 '.prev',
 			speed:		 3000,
 			cleartypeNoBg: 'true'
 		});
-		
-		
-		
+
+
+
 		// Toggle
 		$(".toggle").tabs(".toggle div.pane", {tabs: 'h5', effect: 'slide'});
-		
-		
-		
+
+
+
 		// Tabs
 		$(".tabs").tabs("div.panes > article");
-		
-		
-		
-		// Light Box
-		$(function() {
-			$('a.lightbox').lightBox();
-		});
-		
-		
-		
+
+
+
+		// // Light Box
+		// $(function() {
+		// 	$('a.lightbox').lightBox();
+		// });
+
+
+
 		function equalHeight(group) {
 		var tallest = 0;
 		group.each(function() {
@@ -42,11 +42,11 @@ jQuery(document).ready(function() {
 		});
 		group.height(tallest);
 		}
-		
+
 		equalHeight($(".column"));
-	
-	
-	
+
+
+
 });
 
 
